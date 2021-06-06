3c2999903a13d5924c1d7ddbb6c6e55a91a5f3c9
Jun Kajii <junk4jii@gmail.com>
2021-06-06 20:59:19 -0300

    ✨ chart widget

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
 
 class ScoreCardWidget extends StatelessWidget {
@@ -19,6 +20,7 @@ class ScoreCardWidget extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
+              child: ChartWidget(),
             ),
             Expanded(
               flex: 3,
