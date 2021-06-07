74a8015d0841c614c77e4c946120c3531f9d1990
Jun Kajii <junk4jii@gmail.com>
2021-06-07 10:02:06 -0300

    🐛 fix alignment

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -43,7 +43,7 @@ class AppBarWidget extends PreferredSize {
                   ),
                 ),
                 Align(
-                  alignment: Alignment(0.0, 1.3),
+                  alignment: Alignment(0.0, 1.0),
                   child: ScoreCardWidget(),
                 )
               ],
