3cc295dea15794c8e446caad174c3c82049f1fa9
Jun Kajii <junk4jii@gmail.com>
2021-06-07 19:13:17 -0300

    ✨ add quiz card widget

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,17 +15,11 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBarWidget(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
         child: Column(
           children: [
-            LevelButtonWidget(
-              label: "Fácil",
             SizedBox(
               height: 24,
             ),
-            LevelButtonWidget(
-              label: "Médio",
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -42,14 +37,26 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-            LevelButtonWidget(
-              label: "Difícil",
-            ),
-            LevelButtonWidget(
-              label: "Perito",
             SizedBox(
               height: 24,
             ),
+            Expanded(
+                child: GridView.count(
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              crossAxisCount: 2,
+              children: [
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+              ],
+            ))
           ],
         ),
       ),
