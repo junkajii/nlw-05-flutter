3b19eca9da56120fdca0e5ad4082ebef88bad67a
Jun Kajii <junk4jii@gmail.com>
2021-06-08 13:54:23 -0300

    ✨ use dynamic data

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,65 +1,94 @@
+import 'package:DevQuiz/core/app_colors.dart';
+import 'home_controller.dart';
+import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          user: controller.user!,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: "Fácil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Médio",
+                  ),
+                  LevelButtonWidget(
+                    label: "Difícil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Perito",
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                  child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                children: controller.quizzes!
+                    .map((e) => QuizCardWidget(
+                          title: e.title,
+                          completed:
+                              "${e.questionAnswered}/${e.questions.length}",
+                          percent: e.questionAnswered / e.questions.length,
+                        ))
+                    .toList(),
+              ))
+            ],
+          ),
+        ),
+      );
+    }
+    // else {
     return Scaffold(
-      appBar: AppBarWidget(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                LevelButtonWidget(
-                  label: "Fácil",
-                ),
-                LevelButtonWidget(
-                  label: "Médio",
-                ),
-                LevelButtonWidget(
-                  label: "Difícil",
-                ),
-                LevelButtonWidget(
-                  label: "Perito",
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Expanded(
-                child: GridView.count(
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              crossAxisCount: 2,
-              children: [
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-              ],
-            ))
-          ],
+      body: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(
+            AppColors.darkGreen,
+          ),
         ),
       ),
     );
+    // }
   }
 }
